af7813c22aa0709d85b97236157df2847a2c0f93
AlexWayke <yuhcha2@yandex.ru>
2022-11-14 22:02:56 +0400

    counter for reduceWord function
    function does`t stop, need live counter

--- js/script.js
+++ js/script.js
@@ -1,38 +1,57 @@
 "use strict"
 // Words change =======================
-const textBox = document.querySelector('.change-text').parentElement;
-const text = document.querySelector('.change-text');
-const newText = document.querySelector('.new-text').innerHTML;
+// const textBox = document.querySelector('.change-text').parentElement;
+// const text = document.querySelector('.change-text');
+// const newText = document.querySelector('.new-text').innerHTML;
+
+// console.log(text)
 
 const randomTimer = function(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const reduceText = function() {
-    const changedText = text.innerText.slice(0, -1);
-    text.innerText = changedText;
-    if (changedText.length > 0) {
-        setTimeout(reduceText, randomTimer(50, 100));
-    } else {
-        addWord();        
-    }
-};
 
-const addWord = function(){
-    text.classList.remove('chinese')
-    let i = 0;
+const addWord = function() {
+    console.log("start");
+}
+const reduceText = function(e) {
+    let text = e.firstElementChild;
+    let textCounter = text.innerText.length;
+    let i = 1;
     let addTurn = setInterval(() => {
-        if( i > newText.length) {
-            clearInterval(addTurn)
+        if (i > textCounter) {
+            clearInterval(addTurn);
+            addWord();
         } else {
-            text.innerText = newText.slice(0, i++);
+            let changedText = text.innerText.slice(0, -1);
+            text.innerText = changedText;
+            console.log(textCounter)
         }
-    }, randomTimer(40, 80));
+    },randomTimer(40, 80));
+
 };
 
+// const addWord = function(){
+//     text.classList.remove('chinese')
+//     let i = 0;
+//     let addTurn = setInterval(() => {
+//         if( i > newText.length) {
+//             clearInterval(addTurn)
+//         } else {
+//             text.innerText = newText.slice(0, i++);
+//         }
+//     }, randomTimer(40, 80));
+// };
 
-textBox.addEventListener("click", function(){
-    reduceText();
+
+
+// textBox.addEventListener("click", function(){
+//     // reduceText();
+// })
+// console.log(newText)
+document.addEventListener("click", function(e) {
+    reduceText(e.target)
+    // console.log(e.target)
+    // console.log(e.target.firstElementChild)
+    // console.log(e.target.lastElementChild.innerHTML)
 })
-
-console.log(newText)
