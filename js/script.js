1052c9487964f012d4e03b8508acea1ec1bc60e5
AlexWayke <yuhcha2@yandex.ru>
2022-11-15 17:26:43 +0400

    forEach now works

--- js/script.js
+++ js/script.js
@@ -1,57 +1,46 @@
 "use strict"
 // Words change =======================
-// const textBox = document.querySelector('.change-text').parentElement;
-// const text = document.querySelector('.change-text');
-// const newText = document.querySelector('.new-text').innerHTML;
+const textBox = document.querySelectorAll('.change-box');
 
-// console.log(text)
 
 const randomTimer = function(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-
-const addWord = function() {
-    console.log("start");
-}
 const reduceText = function(e) {
     let text = e.firstElementChild;
-    let textCounter = text.innerText.length;
-    let i = 1;
+    text.parentElement.classList.remove('change-box')
+    let i = 0;
     let addTurn = setInterval(() => {
-        if (i > textCounter) {
+        if (i >= text.innerHTML.length) {
             clearInterval(addTurn);
-            addWord();
+            addText(e);
         } else {
             let changedText = text.innerText.slice(0, -1);
             text.innerText = changedText;
-            console.log(textCounter)
         }
     },randomTimer(40, 80));
-
+    
 };
 
-// const addWord = function(){
-//     text.classList.remove('chinese')
-//     let i = 0;
-//     let addTurn = setInterval(() => {
-//         if( i > newText.length) {
-//             clearInterval(addTurn)
-//         } else {
-//             text.innerText = newText.slice(0, i++);
-//         }
-//     }, randomTimer(40, 80));
-// };
+const addText = function(e) {
+    let text = e.firstElementChild;
+    let newText = e.lastElementChild.innerHTML
+    text.classList.remove('chinese')
+    let i = 0;
+    let addTurn = setInterval(() => {
+        if( i > newText.length) {
+            clearInterval(addTurn)
+        } else {
+            text.innerText = newText.slice(0, i++);
+        }
+    }, randomTimer(40, 80));
+}
 
-
-
-// textBox.addEventListener("click", function(){
-//     // reduceText();
-// })
-// console.log(newText)
-document.addEventListener("click", function(e) {
-    reduceText(e.target)
-    // console.log(e.target)
-    // console.log(e.target.firstElementChild)
-    // console.log(e.target.lastElementChild.innerHTML)
+textBox.forEach( function(e) {
+    e.addEventListener("click", function(e) {
+        if(e.target.classList.contains('change-box')){
+            reduceText(e.target);
+        }
+    });
 })
